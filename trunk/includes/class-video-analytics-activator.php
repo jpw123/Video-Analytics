81cce990c531001aaf036d7caf679e8458c41efc
Jan Wojtasinski <jan@829llc.com>
2015-06-09 16:28:59 -0400

    Init commit.

---
+++ trunk/includes/class-video-analytics-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/jpw123/
+ * @since      1.0.0
+ *
+ * @package    Video_Analytics
+ * @subpackage Video_Analytics/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Video_Analytics
+ * @subpackage Video_Analytics/includes
+ * @author     Jan Wojtasinski <jan@829llc.com>
+ */
+class Video_Analytics_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ trunk/includes/class-video-analytics-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/jpw123/
+ * @since      1.0.0
+ *
+ * @package    Video_Analytics
+ * @subpackage Video_Analytics/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Video_Analytics
+ * @subpackage Video_Analytics/includes
+ * @author     Jan Wojtasinski <jan@829llc.com>
+ */
+class Video_Analytics_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
